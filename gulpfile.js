2b939622210eb9146ebcf5bc9895095ef9042bfa
Pravin <prai@wealthmsi.com>
2016-01-07 15:26:29 -0500

    cleared main.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('serve', ['sass'], function(){
     server:{
       baseDir:'./'
     }
-  });
+  })
   gulp.watch("./src/sass/**/*.scss", ["sass"]);
   gulp.watch("./*.html").on('change', browserSync.reload);
   gulp.watch('./js/**/*.js').on('change', browserSync.reload);
